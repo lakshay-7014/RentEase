1534555ecd28e1ffae4c9313a29a37a186c9f3af
tejaswi755 <tejaswisharma755@gmail.com>
2023-05-15 22:33:01 +0530

    product details upload feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:minor/auth/auth_provider.dart';
+import 'package:minor/screens/pages/home_screen.dart';
 import 'package:minor/screens/pages/welcome_screen.dart';
 import 'package:provider/provider.dart';
 import 'const/string_const.dart';
@@ -12,8 +14,10 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(const MyApp());
 }
 
@@ -36,3 +40,5 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
